tequilaOS/platform_vendor_tequila

Revert "sensitive_pn: Run XML lint against the schema"
This reverts commit 271ba02664306fffc317a7286dbdbf96369568cd.

config/telephony.mk
@@ -1,6 +1,6 @@
 # Sensitive Phone Numbers list
-PRODUCT_PACKAGES += \
-    sensitive_pn.xml
+PRODUCT_COPY_FILES += \
+    vendor/tequila/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
 
 # World APN list
 PRODUCT_PACKAGES += \

AxiomAOSP-WIP/vendor_axiom

config/telephony.mk
@@ -1,6 +1,6 @@
 # Sensitive Phone Numbers list
-PRODUCT_PACKAGES += \
-    sensitive_pn.xml
+PRODUCT_COPY_FILES += \
+    vendor/axiom/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
 
 # World APN list
 PRODUCT_PACKAGES += \