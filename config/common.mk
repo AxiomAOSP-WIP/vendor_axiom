tequilaOS/platform_vendor_tequila

vendor: config: sepolicy: import flipendo/turbo_adapter sepolicy
Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

config/common.mk
@@ -172,6 +172,8 @@ PRODUCT_PACKAGES += \
 
 $(call inherit-product, vendor/tequila/config/version.mk)
 
+$(call inherit-product, vendor/tequila/config/sepolicy.mk)
+
 $(call inherit-product, vendor/google/gms/config.mk)
 
 $(call inherit-product, vendor/google/pixel/config.mk)

AxiomAOSP-WIP/vendor_axiom

config/common.mk
@@ -182,9 +182,8 @@ PRODUCT_PACKAGES += \
 		Launcher3Overlay
 
 
-# Translations
-#PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
-#PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
+$(call inherit-product, vendor/axiom/config/sepolicy.mk)
+
 
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/axiom/build/target/product/security/axiom